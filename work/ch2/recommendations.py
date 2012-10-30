617c29c1b576f78b12b18f819923f7f2ea522c45
Andrew Enfield <aenfield@gmail.com>
2012-10-29 18:55:41 -0700

    Changed function names and variable names to better match what they actually do.
    That is, I made the function names better describe what they're actually
    doing (i.e., 'most_similar_first_level_items' instead of 'top_matches' -
    top matches of what?) and changed variable names to be general to any
    use and more accurate - i.e., we don't call a variable 'person1' when in
    reality what we mean is the 'first-level' item (which might be a user or
    person, in common use).

--- work/ch2/recommendations.py
+++ work/ch2/recommendations.py
@@ -1,55 +1,59 @@
 from math import sqrt
 
-# return a distance-based similarity score for person1 and person2
-def sim_distance(all_preferences, person1, person2):
+# return a distance-based similarity score for two items
+def sim_distance(all_preferences, item1, item2):
 	# first get the list of shared items
 	shared_items = {}
-	for item in all_preferences[person1]:
-		if item in all_preferences[person2]:
+	for item in all_preferences[item1]:
+		if item in all_preferences[item2]:
 			shared_items[item] = 1
 
 	# if there are no shared ratings, they have a 0 similarity
 	if len(shared_items) == 0: return 0
 
 	# distance uses the sum of the squares for the difference in each shared rating
-	sum_of_squares = sum([pow(all_preferences[person1][item] - all_preferences[person2][item],2)
-						  for item in all_preferences[person1] if item in all_preferences[person2]])
+	sum_of_squares = sum([pow(all_preferences[item1][item] - all_preferences[item2][item],2)
+						  for item in all_preferences[item1] if item in all_preferences[item2]])
 
 	# return a value between 0 and 1 by adding one to the sum (to avoid div by 0) and inverting
 	return 1 / (1 + sum_of_squares) 
 
 # return a Pearson similarity score
-def sim_pearson(all_preferences, person1, person2):
+def sim_pearson(all_preferences, item1, item2):
 	shared_items = {}
-	for item in all_preferences[person1]:
-		if item in all_preferences[person2]: shared_items[item] = 1
+	for item in all_preferences[item1]:
+		if item in all_preferences[item2]: shared_items[item] = 1
 
 	num_shared_ratings = len(shared_items)
 
 	if num_shared_ratings == 0: return 0
 
 	# Pearson needs sum of prefs, some of squares of prefs, and sum of products
-	sum_person1 = sum([all_preferences[person1][item] for item in shared_items])
-	sum_person2 = sum([all_preferences[person2][item] for item in shared_items])
+	sum_item1 = sum([all_preferences[item1][item] for item in shared_items])
+	sum_item2 = sum([all_preferences[item2][item] for item in shared_items])
 
-	sumSq_person1 = sum([pow(all_preferences[person1][item],2) for item in shared_items])
-	sumSq_person2 = sum([pow(all_preferences[person2][item],2) for item in shared_items])
+	sumSq_item1 = sum([pow(all_preferences[item1][item],2) for item in shared_items])
+	sumSq_item2 = sum([pow(all_preferences[item2][item],2) for item in shared_items])
 
-	sumProducts = sum([all_preferences[person1][item]*all_preferences[person2][item] for item in shared_items])
+	sumProducts = sum([all_preferences[item1][item]*all_preferences[item2][item] for item in shared_items])
 
 	# now ready to finally calculate the Pearson score
-	numeratorPearson = sumProducts - (sum_person1 * sum_person2 / num_shared_ratings)
-	denominatorPearson = sqrt((sumSq_person1-pow(sum_person1,2)/num_shared_ratings)*(sumSq_person2-pow(sum_person2,2)/num_shared_ratings))
+	numeratorPearson = sumProducts - (sum_item1 * sum_item2 / num_shared_ratings)
+	denominatorPearson = sqrt((sumSq_item1-pow(sum_item1,2)/num_shared_ratings)*(sumSq_item2-pow(sum_item2,2)/num_shared_ratings))
 
 	if denominatorPearson == 0: return 0
 
 	return numeratorPearson / denominatorPearson
 
 
-# return the best - most similar - matches for a particular person
-def top_matches(all_preferences, person, num_of_matches = 5, similarity_func = sim_pearson):
-	scores = [(similarity_func(all_preferences, person, other_person),other_person) 
-		for other_person in all_preferences if other_person != person]
+# Return the best - most similar - first-level item matches for a specified first-level item.
+# If the all_preferences dict is set up with users as first-level items and
+# items (products, say) as second-level items, then this function returns the most similar users;
+# if that same all_preferences dict is transformed so items/products are the first-level
+# items, then this function returns the items most similar to the specified item.
+def most_similar_first_level_items(all_preferences, first_level_item_to_match, num_of_matches = 5, similarity_func = sim_pearson):
+	scores = [(similarity_func(all_preferences, first_level_item_to_match, other_first_level_item), other_first_level_item) 
+		for other_first_level_item in all_preferences if other_first_level_item != first_level_item_to_match]
 
 	# sort resulting list so highest scores are first
 	scores.sort()
@@ -57,47 +61,51 @@ def top_matches(all_preferences, person, num_of_matches = 5, similarity_func = s
 
 	return scores[0:num_of_matches]	
 
-# get recommendations for a person using a weighted average of every other user's
-# rating - so that ratings from someone that's more similar to the person hold
-# more weight when determining a recommendation
-def get_recommendations(all_preferences, person, similarity_func = sim_pearson):
+# Return the best/most similar second-level items for a specified first-level item.
+# If the all_preferences dict is set up with users as first-level items and items/products
+# as second-level items, this function returns the items/products (i.e., second-level items)
+# that are most likely to be interesting to the user. 
+# It does this using a weighted average of every other user's  ratings, so that ratings 
+# from users that are more similar to the specified person (first-level item) hold
+# more weight when determining the recommendation.
+def most_similar_second_level_items(all_preferences, first_level_item, similarity_func = sim_pearson):
 	totals = {}
 	similarity_sums = {}
 
-	for other_person in all_preferences:
-		# don't compare the person to himself
-		if other_person == person: continue
-		how_similar = similarity_func(all_preferences, person, other_person)
+	for other_first_level_item in all_preferences:
+		# don't compare the first-level item to the same first-level item
+		if other_first_level_item == first_level_item: continue
+		how_similar = similarity_func(all_preferences, first_level_item, other_first_level_item)
 
 		# don't bother w/ similarity scores that are zero or less
 		if how_similar <= 0: continue
 
-		for item in all_preferences[other_person]:
-			# only score movies the person hasn't seen
-			if item not in all_preferences[person] or all_preferences[person][item] == 0:
-				totals.setdefault(item, 0)
-				totals[item] += all_preferences[other_person][item] * how_similar
+		for second_level_item in all_preferences[other_first_level_item]:
+			# only score second-level items the first-level item hasn't rated
+			if second_level_item not in all_preferences[first_level_item] or all_preferences[first_level_item][second_level_item] == 0:
+				totals.setdefault(second_level_item, 0)
+				totals[second_level_item] += all_preferences[other_first_level_item][second_level_item] * how_similar
 
-				similarity_sums.setdefault(item, 0)
-				similarity_sums[item] += how_similar
+				similarity_sums.setdefault(second_level_item, 0)
+				similarity_sums[second_level_item] += how_similar
 
 	# create the normalized list of recommendations
-	rankings = [(total / similarity_sums[item],item) for item,total in totals.items()]
+	rankings = [(total / similarity_sums[second_level_item],second_level_item) for second_level_item,total in totals.items()]
 
 	rankings.sort()
 	rankings.reverse()
 	return rankings
 
-# swap first index w/ second index, to enable matches and recommendations on the 
-# second item; i.e., instead of first people and then movies (and matches and recs
+# Swap first index w/ second index, to enable matches and recommendations on the 
+# second-level item; i.e., instead of first people and then movies (and matches and recs
 # based on similar people), we'd have first movies and then people, and can get
-# similarity matches and recommendations based on the similarity between movies
+# similarity matches and recommendations based on the similarity between movies.
 def swap_first_and_second_indexes(all_preferences):
 	swapped_prefs = {}
-	for first_item in all_preferences:
-		for second_item in all_preferences[first_item]:
-			swapped_prefs.setdefault(second_item,{})
-			swapped_prefs[second_item][first_item] = all_preferences[first_item][second_item]
+	for first_level_item in all_preferences:
+		for second_level_item in all_preferences[first_level_item]:
+			swapped_prefs.setdefault(second_level_item,{})
+			swapped_prefs[second_level_item][first_level_item] = all_preferences[first_level_item][second_level_item]
 
 	return swapped_prefs
 
